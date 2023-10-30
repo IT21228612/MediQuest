a85587b17e02c8aeaad65b6b06b12923174ec011
Thimesh Madusanka <thimesh.madusanka19@gmail.com>
2023-10-30 14:43:28 +0530

    IT21196010_app

---
+++ app/src/main/java/com/example/digitaljournal/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.digitaljournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addRecordBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        addRecordBtn = findViewById(R.id.add_record_btn);
+        recyclerView = findViewById(R.id.recyler_view);
+        menuBtn = findViewById(R.id.menu_btn);
+        addRecordBtn.setOnClickListener((v)->startActivity(new Intent(MainActivity.this,RecordDetailsActivity.class)));
+        menuBtn.setOnClickListener((v)->showMenu());
+        setupRecycleView();
+    }
+
+    void showMenu(){
+
+        PopupMenu popupMenu  = new PopupMenu(MainActivity.this,menuBtn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if(menuItem.getTitle()=="Logout"){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this,LoginActivty.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+
+    void setupRecycleView(){
+        Query query  = Utility.getCollectionReferenceForNotes().orderBy("timestamp",Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>().setQuery(query,Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options,this);
+        recyclerView.setAdapter(noteAdapter);
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
+    }
+
+
+
+}
